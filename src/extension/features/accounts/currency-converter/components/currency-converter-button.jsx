eccf43e331edc140e042cb89b62dfa6814df0a94
zalmang <133536986+zalmang@users.noreply.github.com>
2023-06-14 21:14:10 +0300

    no message

--- src/extension/features/accounts/currency-converter/components/currency-converter-button.jsx
+++ src/extension/features/accounts/currency-converter/components/currency-converter-button.jsx
@@ -11,38 +11,45 @@ export class CurrencyConverterButton extends React.Component {
 
   // openCurrencyConverter FOR NOW is only going to adjust one transaction
   openCurrencyConverter = () => {
-    // for future versions: create modal to enter a currency rate
-    console.log('And so it begins...');
-    let rate = 3.75;
-    console.log('Beginning conversion');
-    let row = document.querySelector('.ynab-grid-body-row.is-checked');
-    if (row === false) {
+    let rate = this.getRate();
+    let rows = $('.ynab-grid-body-row.is-checked').toArray();
+    if (rows.length === false) {
       return;
     }
-    row.click();
-    const inflows = $('.is-editing .ynab-grid-cell-inflow .ember-text-field').toArray();
-    const outflows = $('.is-editing .ynab-grid-cell-outflow .ember-text-field').toArray();
-    if (outflows.length !== inflows.length) {
-      console.log('ARRAY LENGTHS ARE NOT THE SAME');
-      return;
-    }
-    for (let i = 0; i < inflows.length; i++) {
-      let currIn = inflows[i];
-      let currOut = outflows[i];
-      if (ynab.unformat(currIn.value) !== 0) {
-        convert(currIn, rate);
-      } else if (ynab.unformat(currOut.value !== 0)) {
-        convert(currOut, rate);
-      } else {
-        console.out('No value here');
+    for (let j = 0; j < rows.length; j++) {
+      let row = rows[j];
+      row.click();
+      const inflows = $('.is-editing .ynab-grid-cell-inflow .ember-text-field').toArray();
+      const outflows = $('.is-editing .ynab-grid-cell-outflow .ember-text-field').toArray();
+      if (outflows.length !== inflows.length) {
+        console.log('ARRAY LENGTHS ARE NOT THE SAME');
+        return;
       }
+      for (let i = 0; i < inflows.length; i++) {
+        let currIn = inflows[i];
+        let currOut = outflows[i];
+        if (ynab.unformat(currIn.value) !== 0) {
+          convert(currIn, rate);
+        } else if (ynab.unformat(currOut.value !== 0)) {
+          convert(currOut, rate);
+        } else {
+          console.out('No value here');
+        }
+      }
+      const $saveButton = $('.ynab-grid-actions-buttons .button.button-primary');
+      $saveButton.trigger('click');
     }
   };
+
+  getRate() {
+    return prompt('Enter current conversion rate');
+  }
 }
 
 export function convert(htmlElement, rate) {
   let currValue = ynab.unformat(htmlElement.value);
   htmlElement.value = ynab.formatCurrency(currValue * rate * 1000);
-  htmlElement.dispactEvent(new Event('change'));
+  // $(this).trigger(jQuery.Event('blur'));
+  htmlElement.dispatchEvent(new Event('change'));
   htmlElement.dispatchEvent(new Event('blur'));
 }
